ada7e6358078fae2884b06ae63d10d6d971325f0
Li-Pro <53344765+Li-Pro@users.noreply.github.com>
2020-05-31 14:29:54 +0800

    Updating setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,42 @@
+#######################
+# check / install pip #
+#######################
+try:
+	import pip
+except:
+	try:
+		import ensurepip
+		ensurepip.bootstrap()
+		
+		import pip
+	except:
+		raise Exception('PIP does not exist, and can not be installed.')
+
+
+###############################
+# Detect version requirements #
+###############################
 import pip
 import sys
 import subprocess
 
 from warnings import warn as _warn
 
-# Detect version requirements
 py_ver = sys.version_info[0: 2]
 pip_ver = tuple(map(int, pip.__version__.split('.')))
 
 req_py_ver = (3, 5)
+suggest_pip_ver = (20, 1)
 if py_ver < req_py_ver:
 	raise Exception('Python version should be >=', req_py_ver)
 
-if pip_ver < (20, 1):
-	_warn('PIP might need to be updated (currently using {})'.format('.'.join(map(str, pip_ver))))
+if pip_ver < suggest_pip_ver:
+	_warn('PIP might need to be upgraded (currently using {})'.format('.'.join(map(str, pip_ver))))
 
-# Setup from requirements
+
+###########################
+# Setup from requirements #
+###########################
 APT_INST = ['sudo', 'apt-get', 'install']
 PIP_INST = [sys.executable, '-m', 'pip', 'install']
 
@@ -23,16 +44,19 @@ COMMON_DEP = [
 	[*PIP_INST, 'Pillow==7.1.2']
 ]
 
+for depends in COMMON_DEP:
+	subprocess.run(depends)
+
+
+######################################
+# Install additional files if needed #
+######################################
 LINUX_DEP = [
 	[*APT_INST, 'xclip', 'python3-tk']
 ]
 
 WIN_DARWIN_DEP = [
 ]
-
-# Install additional files if needed
-for depends in COMMON_DEP:
-	subprocess.run(depends)
 
 if sys.platform in ('darwin', 'win32'):
 	for depends in WIN_DARWIN_DEP:
@@ -46,3 +70,6 @@ else:
 	_warn('Platform other than Windows, macOS and Linux might be unsupported.')
 	for depends in LINUX_DEP:
 		subprocess.run(depends)
+
+print()
+print('Setup completed.')
