329be81902d6fc5e29e60c85d660a86055e5c3ac
Li-Pro <53344765+Li-Pro@users.noreply.github.com>
2020-07-29 22:50:16 +0800

    Adding uidemo.py

--- demo.py
+++ demo.py
@@ -17,20 +17,7 @@ def imgCompare(img1, img2):
 	if not img2.size == (h, w):
 		return False
 	
-	# print('#', h, w, h*w, len(img1.tobytes()), len(img2.tobytes()))
-	# exit(0)
-	# return True
-	
-	# for i in range(h):
-		# for j in range(w):
-			# if not img1.getpixel((i, j)) == img2.getpixel((i, j)):
-				# return False
-	
-	# return True
 	return img1.tobytes() == img2.tobytes()
-
-def captureProc():
-	return
 
 def main():
 	iwdir = input('Working directory: ')
@@ -54,45 +41,26 @@ def main():
 	## Wait for input
 	lstimg = None
 	while True:
-		# nowtime = time.perf_counter()
 		try:
 			img = getClipboardImage()
-			# print('#getClipboardImage: {}'.format(time.perf_counter() - nowtime))
-			# nowtime = time.perf_counter()
-			
+		
 		except NoImageData:
 			time.sleep(.1)
-			# print('#NoImageData: {}'.format(time.perf_counter() - nowtime))
-			# nowtime = time.perf_counter()
-			
 			continue
 		
 		except (KeyboardInterrupt, SystemExit):
 			break
 		
 		else:
-			# print('#1: {}'.format(time.perf_counter() - nowtime))
-			# nowtime = time.perf_counter()
-			
 			if imgCompare(img, lstimg):
-				# print('#imgCompare: {}'.format(time.perf_counter() - nowtime))
-				# nowtime = time.perf_counter()
-				
 				time.sleep(.2)
 				continue
 			
-			# print('#2: {}'.format(time.perf_counter() - nowtime))
-			# nowtime = time.perf_counter()
-			
 			img_path = wdir / '{}.png'.format(wdcnt)
 			img.save(img_path.resolve())
-			# print('#img.save: {}'.format(time.perf_counter() - nowtime))
-			# nowtime = time.perf_counter()
 			
 			wdcnt += 1
 			lstimg = img
-			# print('#img =: {}'.format(time.perf_counter() - nowtime))
-			# nowtime = time.perf_counter()
 		
 		finally:
 			with wdinfo.open('w') as file:
